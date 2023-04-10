9393d61f1419f51d7217977a910956125e9ac867
DENGARDEN <poowooho3@g.skku.edu>
2023-04-10 22:28:52 +0900

    solved with dask bag map & reduce

--- toolkit/barcode_extraction_from_constant_region/barcode_extractor.py
+++ toolkit/barcode_extraction_from_constant_region/barcode_extractor.py
@@ -5,6 +5,7 @@ import dask.array as da
 import dask.bag as db
 import re
 from concurrent.futures import ProcessPoolExecutor
+import numpy as np
 from multiprocessing import cpu_count
 
 UPSTREAM_FLANKING_SEQUENCE = "TCTTTTTACTGACTGCAGTCTGAGTCTGACAG"
@@ -13,33 +14,34 @@ DOWNSTREAM_FLANKING_SEQUENCE = "AGCAGAGCTACGCACTCTATGCTAGCTCGA"
 SEARCH_PATTERN = rf"({UPSTREAM_FLANKING_SEQUENCE})(.*)({DOWNSTREAM_FLANKING_SEQUENCE})"
 
 
-# @dask.delayed
-# def parse(block):
-#     return np.genfromtxt(io.BytesIO(*block), usecols=0)
+def pattern_matcher(pat, seq):
+    result = re.search(pat, seq, flags=re.IGNORECASE)
+    return result.group(2) if result else np.nan
 
 
 def extract_barcodes(src_file: pathlib.Path, dest_dir: pathlib.Path) -> None:
-    bags = db.read_text(str(src_file), blocksize=4**10)
-    arr = da.stack([bag for bag in bags], axis=0).reshape(-1, 4)
+    bags = db.read_text(str(src_file), blocksize="256MB")
 
+    # 2*n + 1
     # id, seq, +, qual
-    seqs = dd.from_dask_array(arr[:, 1])
+    bags = bags.filter(lambda x: re.match(rf"^(A|T|G|C|a|t|g|c)", x))
+    barcodes = bags.map(
+        lambda x: pattern_matcher(pat=SEARCH_PATTERN, seq=x)
+    ).to_dataframe(meta={0: "object"})
 
-    query_result = seqs.str.extract(SEARCH_PATTERN, flags=re.IGNORECASE)
     with open(dest_dir / f"{src_file.stem}_stat.csv", "w") as s:
-        detected, total_read, detection_rate, unique = (
-            query_result[1].count().compute(),
-            seqs.shape[0].compute(),
-            query_result[1].count().compute() / seqs.shape[0].compute(),
-            query_result[1].nunique().compute(),
+        detected, total_read = (
+            barcodes.count().compute(),
+            barcodes.shape[0].compute(),
         )
+        detection_rate = detected / total_read
+
         s.write(f"File: {src_file.name}\n")
-        s.write(f"Unique barcodes found: {unique}\n")
         s.write(f"Total read: {total_read}\n")
         s.write(f"Detected read: {detected}\n")
         s.write(f"Detection rate in the sequence pool: {detection_rate}\n")
 
-    query_result.drop_duplicates(1)[1].dropna().compute().to_csv(
+    barcodes.dropna().drop_duplicates().reset_index(drop=True).compute().to_csv(
         dest_dir / f"{src_file.stem}_barcode.csv", index=True, header=False
     )
 
@@ -48,8 +50,7 @@ if __name__ == "__main__":
     src_files = pathlib.Path("./src/").glob("*.fastq")
     dest_dir = pathlib.Path("./dest/")
 
-    with ProcessPoolExecutor(max_workers=1) as executor:
+    with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
         for src_file in src_files:
-            extract_barcodes(src_file=src_file, dest_dir=dest_dir)
-
-            # executor.submit(extract_barcodes, src_file, dest_dir).result()
+            # extract_barcodes(src_file, dest_dir)  # debug
+            executor.submit(extract_barcodes, src_file, dest_dir)
